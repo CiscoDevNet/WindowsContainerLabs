efabf932f6b00d24afac2455478c830f55f571fb
Israel Ogbole <iogbole@gmail.com>
2020-05-26 20:04:42 +0100

    k8s args support

--- asp-dotnet-mvc-biq/container/ENTRYPOINT.ps1
+++ asp-dotnet-mvc-biq/container/ENTRYPOINT.ps1
@@ -4,4 +4,7 @@
 & "C:\AppDynamics\DotNetAgent\GenerateLoad.ps1"
 
 # Any args from K8s spec can be read using the format below
-${env:ARGSNAME} 
+$arguments = "" 
+foreach ($arg in $args) {
+    $arguments = $arguments + " " + $arg
+}
--- dotnet-console-no-service/container/ENTRYPOINT.ps1
+++ dotnet-console-no-service/container/ENTRYPOINT.ps1
@@ -2,8 +2,19 @@
 # Call AppD Bootstrapper before your appplication starts up
 & "C:\AppDynamics\DotNetAgent\BootstrapAppD.ps1"
 
-# This will typically be the customer's custom point command.
+# Any args from K8s deployment can be read using the format below
+$arguments = "" 
+foreach ($arg in $args) {
+    $arguments = $arguments + " " + $arg
+}
 
+# This will typically be the customer's custom point command. with the Args from K8s deployment - if any 
+# Start-Process C:\resource-check\DockerOnWindows.ResourceCheck.Console.exe -ArgumentList "$arguments"
+
+#Because we've changed the entrypoint, kubectl logs command will no work as intended, fix it by tailing the logs 
+#Get-Content C:\resource-check\log.out -tail 10 
+
+#### This section is specific to this demo app, do not use in a customer environment. 
 while(1){
   $timenow = get-date -Format u
   $dcmd = C:\resource-check\DockerOnWindows.ResourceCheck.Console.exe
