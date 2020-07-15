b85aec5c42fabcfd161fea293de2f391dc45d486
anoop7181 <anoopc7181@outlook.com>
2020-07-15 11:27:52 +0100

    Updated Env list

--- asp-dotnet-mvc-2/container/ENTRYPOINT.ps1
+++ asp-dotnet-mvc-2/container/ENTRYPOINT.ps1
@@ -15,10 +15,10 @@ if ($applicationPoolName -eq ""){
 if ($agentFolderLocation -eq ""){
     $agentFolderLocation = "C:/AppDynamics/"
 }
-C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$applicationPoolName'].environmentVariables.[name='COR_ENABLE_PROFILING',value='1']" /commit:apphost
-C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$applicationPoolName'].environmentVariables.[name='COR_PROFILER',value='{39AEABC1-56A5-405F-B8E7-C3668490DB4A}']" /commit:apphost
-C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$applicationPoolName'].environmentVariables.[name='COR_PROFILER_PATH_32',value='$agentFolderLocation/AppDynamics.Profiler_x86.dll']" /commit:apphost
-C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$applicationPoolName'].environmentVariables.[name='COR_PROFILER_PATH_64',value='$agentFolderLocation/AppDynamics.Profiler_x64.dll']" /commit:apphost
+C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$env:APPDYNAMICS_APPLICATION_POOL_NAME'].environmentVariables.[name='COR_ENABLE_PROFILING',value='1']" /commit:apphost
+C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$env:APPDYNAMICS_APPLICATION_POOL_NAME''].environmentVariables.[name='COR_PROFILER',value='{39AEABC1-56A5-405F-B8E7-C3668490DB4A}']" /commit:apphost
+C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$env:APPDYNAMICS_APPLICATION_POOL_NAME''].environmentVariables.[name='COR_PROFILER_PATH_32',value='$agentFolderLocation/AppDynamics.Profiler_x86.dll']" /commit:apphost
+C:\Windows\system32\inetsrv\appcmd.exe set config -section:system.applicationHost/applicationPools /+"[name='$env:APPDYNAMICS_APPLICATION_POOL_NAME''].environmentVariables.[name='COR_PROFILER_PATH_64',value='$agentFolderLocation/AppDynamics.Profiler_x64.dll']" /commit:apphost
 
 iisreset
 
