cc41ffa864388b956a30ea5177bfc433aa4902e3
Israel Ogbole <iogbole@yahoo.com>
2020-07-17 16:59:23 +0100

    Update build.ps1

--- analytics-agent-windows/container/build.ps1
+++ analytics-agent-windows/container/build.ps1
@@ -4,19 +4,28 @@ Param (
     [string]$agentVersion,
 
     [Parameter(Mandatory = $false)]
-    [string]$dockerHubHandle
+    [string]$dockerHubHandle,
+
+    [Parameter(Mandatory = $false)]
+    [string]$winTag
 )
 
 if ($dockerHubHandle -eq "") {
     $dockerHubHandle = "appdynamics"
  }
-#$IMAGE_NAME = "iogbole/machine-agent-windows-64bit"
+
+if ($winTag -eq "") {
+    $winTag = "win-nano1809"
+ }
+
 $IMAGE_NAME = "$dockerHubHandle/analytics-agent"
 
 Write-Host "version = $agentVersion "
 Write-Host "dockerHubHandle = $dockerHubHandle "
+Write-Host "winTag = $winTag "
 
-docker build --no-cache --build-arg APPD_AGENT_VERSION=$agentVersion -t ${IMAGE_NAME}:$agentVersion-win . 
+docker build --no-cache --build-arg APPD_AGENT_VERSION=$agentVersion -t ${IMAGE_NAME}:$agentVersion-$winTag . 
+ 
 
 #docker run -d --env-file env.list.local ${IMAGE_NAME}:$agentVersion
 #docker push iogbole/windows_analytics_agent:$agentVersion
