797460d94832d5e7636b80b246e32e29045cde97
Israel Ogbole <iogbole@gmail.com>
2020-07-23 19:33:35 +0100

    Update start-agent.ps1

--- analytics-agent-windows/container/start-agent.ps1
+++ analytics-agent-windows/container/start-agent.ps1
@@ -35,9 +35,27 @@ else {
     $analytics_path = "c:/appdynamics/analytics-agent-bundle-64bit-windows"
     Start-Process $analytics_path/jre/bin/java -ArgumentList "-jar $analytics_path/bin/tool/tool-executor.jar start" 
 
-    # Make logs available in docker  logs when it's started up, takes about 10 -15 seconds
-    Start-Sleep -s 20 
-    Get-Content -Path "$analytics_path/logs/analytics-agent.log" -Tail  10 -Wait
+    # Make logs available in docker  logs when it's up, takes about 10 -15 seconds if everything is OK 
+    Start-Sleep -s 30
+    while (!(Test-Path "$analytics_path/logs/analytics-agent.log")) { 
+        $counter++
+        Write-Host "Analytics agent is not up yet, there might be an issue. Please note that it requires a minimum of 1GB heap space to start. Please contact support@appadynamics.com if the issue persists". 
+        Write-Host "The analytics agent container will continue to run to prevent the entire pod from crashing - to ensure your main container continues to server its purpose"
+        Write-Host "The above decision was made on the basis that the standalone analytics agent is mostly deployed as a sidecar container"
+        Write-Host ""
+        Write-Host ""
+
+        #sleep for 3.5 minutes and try again 
+        Start-Sleep -s 20
+        if ($counter -eq 3 -or $counter -eq 6 -or $counter -eq 9) {
+          # Give it a try again, it normally fails due to insufficient heap, could be that things have settled after ~10 minutes. 
+           Write-Host "attempting to start analytics agent again..."
+          Start-Process $analytics_path/jre/bin/java -ArgumentList "-jar $analytics_path/bin/tool/tool-executor.jar start" 
+          # then wait to see if it comes up before testing again.. 
+          Start-Sleep -s 20 
+        }
+    } 
+    Get-Content -Path "$analytics_path/logs/analytics-agent.log" -Tail  10 -Wait -ErrorAction Continue
 }
  
  
