ffa65537d02486a94b70500075e91bcffdecdb2d
alex_jov <aleksjovovic@gmail.com>
2020-08-12 13:30:29 +0100

    Db agent use Deployment; Remove Service and ports; Use servercore;

--- db-agent-windows/container/build.ps1
+++ db-agent-windows/container/build.ps1
@@ -15,22 +15,23 @@ if ($dockerHubHandle -eq "") {
 }
 
 if ($winTag -eq "") {
-    $winTag = "win-nano"
+    $winTag = "win-core"
  }
 
 $imageName = "$dockerHubHandle/db-agent"
 
 Write-Host "version = $agentVersion "
 Write-Host "dockerHubHandle = $dockerHubHandle "
+Write-Host "winTag = $winTag "
 
 # Build
 docker build --no-cache --build-arg APPDYNAMICS_AGENT_VERSION=$agentVersion -t ${imageName}:$agentVersion-$winTag . 
 
 # Run (with env)
-docker run -d --env-file env.list.local ${imageName}:$agentVersion
+#docker run -d --env-file env.list.local ${imageName}:$agentVersion-$winTag
 # Push
-#docker push ${imageName}:$agentVersion
+#docker push ${imageName}:$agentVersion-$winTag
 
-# docker image inspect --format='' alexappd/db-agent:20.5.1
+# docker image inspect --format='' alexappd/db-agent:20.5.1-win-nano
 # docker exec -it container_id powershell 
 # ./build.ps1 -agentVersion "20.7.0.1892" -dockerHubHandle alexappd    
