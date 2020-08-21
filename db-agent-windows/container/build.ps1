4d23aaac89301ae8fe1f20cdc708a0457e8b80a9
alex_jov <aleksjovovic@gmail.com>
2020-08-21 19:06:53 +0100

    Use nano; Run agent with Java; k8s image name update;

--- db-agent-windows/container/build.ps1
+++ db-agent-windows/container/build.ps1
@@ -15,7 +15,7 @@ if ($dockerHubHandle -eq "") {
 }
 
 if ($winTag -eq "") {
-    $winTag = "win-core"
+    $winTag = "win-nano"
  }
 
 $imageName = "$dockerHubHandle/db-agent"
@@ -24,14 +24,17 @@ Write-Host "version = $agentVersion "
 Write-Host "dockerHubHandle = $dockerHubHandle "
 Write-Host "winTag = $winTag "
 
-# Build
+# 1. Build
 docker build --no-cache --build-arg APPDYNAMICS_AGENT_VERSION=$agentVersion -t ${imageName}:$agentVersion-$winTag . 
 
-# Run (with env)
-#docker run -d --env-file env.list.local ${imageName}:$agentVersion-$winTag
-# Push
+# 2. Run (with env)
+docker run -d --env-file env.list.local ${imageName}:$agentVersion-$winTag
+
+# 3. Push
 #docker push ${imageName}:$agentVersion-$winTag
 
-# docker image inspect --format='' alexappd/db-agent:20.5.1-win-nano
-# docker exec -it container_id powershell 
+# docker run -d --env-file env.list.local alexappd/db-agent:20.7.0.1892-win-nano
+# docker push alexappd/db-agent:20.7.0.1892-win-nano
+# core: docker exec -it container_id powershell 
+# nano: docker exec -it container_id pwsh
 # ./build.ps1 -agentVersion "20.7.0.1892" -dockerHubHandle alexappd    
--- db-agent-windows/container/start-agent.ps1
+++ db-agent-windows/container/start-agent.ps1
@@ -1,50 +1,22 @@
 
 if ([string]::IsNullOrEmpty(${env:APPDYNAMICS_DB_AGENT_HOME})) {
    $DB_HOME = "c:\\appdynamics\\dbagent"
-}else{
+} else {
    $DB_HOME = "${env:APPDYNAMICS_DB_AGENT_HOME}"
 }
 
-if ([string]::IsNullOrEmpty(${env:APPDYNAMICS_AGENT_VERSION})) {
-   $Agent_Version_Error = [string]"Agent version not found. Function aborting..."
-   Write-Error $Agent_Version_Error
-   return
+if ([string]::IsNullOrEmpty(${env:JAVA_HOME})) {
+   $JAVA_HOME = "c:\\appdynamics\\dbagent\\jre"
 } else {
-   $AGENT_VERSION = "${env:APPDYNAMICS_AGENT_VERSION}"
+   $JAVA_HOME = "${env:JAVA_HOME}"
 }
 
-Write-Host "APPDYNAMICS_DBAGENT_HOME"
-Write-Host ${env:APPDYNAMICS_DB_AGENT_HOME}
+$JAVA_FILE_PATH=[string]::Format("{0}\\bin\\java.exe",$JAVA_HOME)
 
-#$Url = "https://download.appdynamics.com/download/prox/download-file/db-agent-winx64/$AGENT_VERSION/db-agent-64bit-windows-$AGENT_VERSION.zip"
-$OutputFile = "$DB_HOME\db-agent-64bit-windows-$AGENT_VERSION.zip"
+# start database agent
+Start-Process -FilePath $JAVA_FILE_PATH -ArgumentList "-Djava.library.path=""$DB_HOME\auth\x64"" -jar $DB_HOME\\db-agent.jar"   
 
-Write-Host "OutputFile:"
-Write-Host ${OutputFile}
-
-#Invoke-RestMethod -Method 'Get' -Uri $Url -OutFile "$OutputFile"
-
-Expand-Archive -Path $OutputFile -DestinationPath "$DB_HOME"
-
-#Remove-Item -Path $OutputFile -Force
-
-# Start DB Agent as Windows Service
-cscript "$DB_HOME\InstallService.vbs" "-""Dappdynamics.controller.hostName""=${env:APPDYNAMICS_CONTROLLER_HOST_NAME} 
--""Dappdynamics.controller.port""=""${env:APPDYNAMICS_CONTROLLER_PORT}"" 
--""Dappdynamics.agent.accountName""=""${env:APPDYNAMICS_AGENT_ACCOUNT_NAME}""
--""Dappdynamics.agent.accountAccessKey""=${env:APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY}
--""Dappdynamics.controller.ssl.enabled""=${env:APPDYNAMICS_CONTROLLER_SSL_ENABLED}"
-
-# cscript "C:\appdynamics\dbagent\InstallService.vbs" '%$DB_PROPERTIES%'
-# Get-Content -Path "C:\appdynamics\dbagent\logs\agent.log" -Tail 50 -Wait
-# cscript "C:\appdynamics\dbagent\UninstallService.vbs"
-
-# this let you do 'k logs'
-Start-Sleep -s 20
-
-Write-Host "Service:"
-Get-Service "Appdynamics*"
-
-Write-Host "Logs:"
-Get-Content -Path "$DB_HOME\logs\agent.log" -Tail 10 -Wait
+# Make logs available in docker when it's started up, takes about 10 -15 seconds
+Start-Sleep -s 30
+Get-Content -Path "$DB_HOME\logs\agent.log" -Tail 10 -Wait -ErrorAction Continue
 
